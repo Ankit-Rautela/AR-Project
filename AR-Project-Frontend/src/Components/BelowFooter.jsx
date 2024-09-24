353ef6a173905d3085624e1350e2222ca9a1e4bb
Ankit Rautela <ankitrautela044@gmail.com>
2024-09-25 03:53:20 +0530

    Update UI

--- AR-Project-Frontend/src/Components/BelowFooter.jsx
+++ AR-Project-Frontend/src/Components/BelowFooter.jsx
@@ -5,12 +5,12 @@ var currentDate = new Date();
 const BelowFooter = () => {
   return (
     <>
-      <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full p-4 text-center bg-white ">
         <div>
-          <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-3xl font-bold text-gray-900 ">
             Made with ❤️ -Ankit Rautela
           </h5>
-          <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+          <p className="mb-5 text-base text-gray-500 sm:text-lg ">
             Let's connect! Reach out to me and let's bring your ideas to life
             together.
           </p>
