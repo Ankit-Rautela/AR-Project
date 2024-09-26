4e845fd3e627a7bf8151f56fe4f8338a5ec8224e
Ankit Rautela <ankitrautela044@gmail.com>
2024-09-27 00:42:50 +0530

    fix

--- AR-Project-Frontend/src/store/authStore.js
+++ AR-Project-Frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api/auth" : "https://ar-project-1.onrender.com/api/auth";
 
 axios.defaults.withCredentials = true;
 
